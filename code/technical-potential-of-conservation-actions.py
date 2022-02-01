6136a9b990c32ad41244ec9a2689bdbe366e4d40
Carla Archibald <c.archibald@deakin.edu.au>
2022-02-02 10:51:14 +1100

    Initial script comit

---
+++ code/technical-potential-of-conservation-actions.py
@@ -0,0 +1,19 @@
+# !/bin/env python3
+#
+# technical-potential-of-conservation-actions.py 
+#
+# This script scopes the utility of "technical abatement/mitigation potential" analysis to quantify the potential 
+# of different conservation action to meet conservation and sustainability targets.
+# 
+# By Carla Arcihbald (c.archibaldc@deakin.edu.au)
+# Created: 2022-02-02
+# Last modified: 2022-02-02
+#
+#
+################################################
+
+################ Set up ########################
+# Load python modules
+import numpy as np
+import pandas as pd
+
